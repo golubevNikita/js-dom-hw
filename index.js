5ae3dbb7fae12b25b65f93c279e8848ddfc8227e
golubevNikita <vulivercushe.premedu@gmail.com>
2025-01-19 02:11:37 +0300

    completed-hw4

--- index.js
+++ index.js
@@ -1,90 +1,15 @@
-export const nameInput = document.querySelector('[data-js-name-input]')
-export const commentInput = document.querySelector('[data-js-comment-input]')
-export const addCommentButton = document.querySelector('[data-js-add-button]')
-export const commentContainer = document.querySelector(
-    '[data-js-comment-container]',
-)
-export const commentText = document.querySelector('[data-js-text-area]')
-
-import { commentsArray } from './modules/commentsInfoArr.js'
 import { commentRender } from './modules/renderCommentsFunctions.js'
-import { correctInput } from './modules/inputProcessingFunctions.js'
-import { delay } from './modules/apiImitation.js'
+import { newContentOfArray } from './modules/commentsInfoArr.js'
+import { apiRequest } from './modules/apiImitation.js'
 
 const commentsRenderingInfo = document.querySelector(
     '[data-js-comments-rendering-info]',
 )
 
-delay(1000).then(() => {
-    commentsRenderingInfo.remove()
-    commentRender()
-})
-
-addCommentButton.addEventListener('click', () => {
-    commentInput.classList.remove('main__input_empty')
-    nameInput.classList.remove('main__input_empty')
-
-    let newDateObject = new Date()
-    const correctedYear = String(newDateObject.getFullYear())
-        .split('')
-        .slice(-2)
-        .join('')
-
-    const commentDate = `${newDateObject.getDate()}.${newDateObject.getMonth() + 1}.${correctedYear}
-      ${newDateObject.getHours()}:${newDateObject.getMinutes()}`
-
-    if (commentInput.value === '' && nameInput.value === '') {
-        commentInput.classList.add('main__input_empty')
-        nameInput.classList.add('main__input_empty')
-        return
-    }
-
-    if (commentInput.value === '') {
-        commentInput.classList.add('main__input_empty')
-
-        return
-    }
-
-    if (nameInput.value === '') {
-        nameInput.classList.add('main__input_empty')
-        return
-    }
-
-    const newCommentsArray = {
-        commentHeader: {
-            himself: correctInput(nameInput),
-            commentDateAndTime: commentDate,
-        },
-
-        commentBody: {
-            commentText: correctInput(commentInput),
-        },
-
-        commentFooter: {
-            likes: {
-                likesCounter: 0,
-                likeButton: false,
-            },
-        },
-    }
-
-    const addingCommentinformation = document.querySelector(
-        '[data-js-comment-adding-info]',
-    )
-
-    const formContainer = document.querySelector('[data-js-add-form-container]')
-
-    addingCommentinformation.style.display = 'block'
-    formContainer.style.visibility = 'hidden'
-
-    delay(1000).then(() => {
-        commentsArray.push(newCommentsArray)
-
-        commentInput.value = ''
-        nameInput.value = ''
-        addingCommentinformation.style.display = 'none'
-        formContainer.style.visibility = 'visible'
-
+apiRequest('https://wedev-api.sky.pro/api/v1/golubev-nikita/comments').then(
+    (response) => {
+        commentsRenderingInfo.remove()
+        newContentOfArray(response)
         commentRender()
-    })
-})
+    },
+)
--- modules/commentsInfoArr.js
+++ modules/commentsInfoArr.js
@@ -1,37 +1,36 @@
-export const commentsArray = [
-    {
-        commentHeader: {
-            himself: 'Варвара Н.',
-            commentDateAndTime: '13.02.22 19:22',
-        },
-
-        commentBody: {
-            commentText: 'Мне нравится как оформлена эта страница! ❤',
-        },
-
-        commentFooter: {
-            likes: {
-                likesCounter: 75,
-                likeButton: true,
-            },
-        },
-    },
-
-    {
-        commentHeader: {
-            himself: 'Глеб Фокин',
-            commentDateAndTime: '12.02.22 12:18',
-        },
-
-        commentBody: {
-            commentText: 'Это будет первый комментарий на этой странице',
-        },
-
-        commentFooter: {
-            likes: {
-                likesCounter: 8,
-                likeButton: false,
-            },
-        },
-    },
+export let commentsArray = [
+    // {
+    //     commentHeader: {
+    //         himself: 'Варвара Н.',
+    //         commentDateAndTime: '13.02.22 19:22',
+    //     },
+    //     commentBody: {
+    //         commentText: 'Мне нравится как оформлена эта страница! ❤',
+    //     },
+    //     commentFooter: {
+    //         likes: {
+    //             likesCounter: 75,
+    //             likeButton: true,
+    //         },
+    //     },
+    // },
+    // {
+    //     commentHeader: {
+    //         himself: 'Глеб Фокин',
+    //         commentDateAndTime: '12.02.22 12:18',
+    //     },
+    //     commentBody: {
+    //         commentText: 'Это будет первый комментарий на этой странице',
+    //     },
+    //     commentFooter: {
+    //         likes: {
+    //             likesCounter: 8,
+    //             likeButton: false,
+    //         },
+    //     },
+    // },
 ]
+
+export function newContentOfArray(newData) {
+    commentsArray = newData
+}
